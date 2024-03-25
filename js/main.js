7d2aea94303538c436db40b5649123db850c2abd
TeaMurchik <teaplum@yandex.ru>
2024-03-25 23:08:01 +0300

    сделано 1задание из 9 модуля

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 import {fragment} from './photo-template.js';
+import {closeFormLoadByClick} from './valid-form.js';
 import { loadPopup, closePopupByClick, loadComment } from './photo-popup.js';
 
 const pictureArea = document.querySelector('.pictures');
 const pictures = document.querySelector('.pictures');
 const cancelBtn = document.querySelector('.big-picture__cancel');
+const cancelLoadImg = document.querySelector('.img-upload__cancel');
 const commentLoadBtn = document.querySelector('.social__comments-loader');
 
 pictureArea.appendChild(fragment);
 
 pictures.addEventListener('click', loadPopup);
 cancelBtn.addEventListener('click', closePopupByClick);
+cancelLoadImg.addEventListener('click', closeFormLoadByClick);
 commentLoadBtn.addEventListener('click', loadComment);
--- js/photo-popup.js
+++ js/photo-popup.js
@@ -18,9 +18,11 @@ function closePopupByClick() {
 }
 
 function loadPopup(evt) {
+  if (evt.target.tagName !== 'IMG') {
+    return;
+  }
   document.addEventListener('keydown', closePopup);
   evt.preventDefault();
-
   const photoTarget = (findObject(objects, evt.target.src));
 
   document.querySelector('.big-picture').classList.remove('hidden');
@@ -35,23 +37,23 @@ function loadPopup(evt) {
     comment.querySelector('.social__text').textContent = photoTarget.comments.message();
   });
   document.querySelector('.social__comments').innerHTML = '';
-  for(let i = 0; i < 5; i++){
+  for (let i = 0; i < 5; i++) {
     document.querySelector('.social__comments').innerHTML += `<li class="social__comment">
-    <img class="social__picture" src="${objects[i].comments.avatar()}" alt="${objects[i].comments.name()}" width="35" height="35">
-      <p class="social__text">${objects[i].comments.message()}</p>
-    </li>`;
+      <img class="social__picture" src="${objects[i].comments.avatar()}" alt="${objects[i].comments.name()}" width="35" height="35">
+        <p class="social__text">${objects[i].comments.message()}</p>
+      </li>`;
   }
   document.body.classList.add('modal-open');
 }
 
-function loadComment(){
+function loadComment() {
   const commentCount = document.querySelector('.social__comment-total-count').textContent;
   const shownCount = document.querySelector('.social__comment-shown-count');
-  if(+shownCount.textContent >= +commentCount){
+  if (+shownCount.textContent >= +commentCount) {
     return;
   }
-  if((+commentCount - +shownCount.textContent) < 5){
-    for (let i = 0; i < (+commentCount - +shownCount.textContent); i++){
+  if ((+commentCount - +shownCount.textContent) < 5) {
+    for (let i = 0; i < (+commentCount - +shownCount.textContent); i++) {
       document.querySelector('.social__comments').innerHTML += `<li class="social__comment">
       <img class="social__picture" src="${objects[i].comments.avatar()}" alt="${objects[i].comments.name()}" width="35" height="35">
       <p class="social__text">${objects[i].comments.message()}</p>
@@ -59,7 +61,7 @@ function loadComment(){
     }
     shownCount.textContent = +shownCount.textContent + (+commentCount - +shownCount.textContent);
   } else {
-    for(let i = 0; i < 5; i++){
+    for (let i = 0; i < 5; i++) {
       document.querySelector('.social__comments').innerHTML += `<li class="social__comment">
       <img class="social__picture" src="${objects[i].comments.avatar()}" alt="${objects[i].comments.name()}" width="35" height="35">
       <p class="social__text">${objects[i].comments.message()}</p>
@@ -69,4 +71,4 @@ function loadComment(){
   }
 }
 
-export {loadPopup, closePopupByClick, loadComment};
+export { loadPopup, closePopupByClick, loadComment, closePopup };
